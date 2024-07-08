83117297e3f8fc3cb09ab1913484f15cdddc0672
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-07-08 15:31:37 -0300

    chore: Update routing configuration to include new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path:'list', component: ListRenderComponent},
+  {path: 'change', component: ChangeNumberComponent},
+  {path:'event', component: EventosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
